eac8f12e5bef7f07ff3b0fb29484abe616810b71
Marie86-alt <132057482+Marie86-alt@users.noreply.github.com>
2026-01-04 15:19:28 +0100

    Add data models initialization in __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+"""
+Data models for the application
+"""
+from .payment import PaymentIntentCreate
+from .status import StatusCheck, StatusCheckCreate
+
+__all__ = ["PaymentIntentCreate", "StatusCheck", "StatusCheckCreate"]
